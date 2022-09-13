a709cb3a73193e6fde6a8f225b2bd0523b127bb6
francesco.miceli <francescomiceli@outlook.com>
2022-09-13 17:59:18 +0200

    esercizio+bonus1

--- js/script.js
+++ js/script.js
@@ -1,38 +1,5 @@
 console.log('JS-OK')
 
-const slides = [
-    {
-        image: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    },
-    {
-        image: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum.',
-    },
-    {
-        image: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        image: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-    },
-    {
-        image: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-    }
-];
-
-console.log(slides);
-
-
-
-
 const app = new Vue({
     el: '#app',
     data:{
@@ -81,6 +48,14 @@ const app = new Vue({
             } else {
                 this.imageActive--;
             }
-        }
+        },
+
+        //BONUS 1
+        bigImage (i){
+            this.imageActive = i;
+        },
+
+
+
     }
 })
